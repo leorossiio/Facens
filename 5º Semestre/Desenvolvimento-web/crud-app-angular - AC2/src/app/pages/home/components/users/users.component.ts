0ad490fda2739b28359e9f10c029093e70e774cf
Leonardo Rossi <83621860+leorossiio@users.noreply.github.com>
2024-04-14 19:02:19 -0300

    feat: adds new functions to user component to edit columns

--- 5º Semestre/Desenvolvimento-web/crud-app-angular - AC2/src/app/pages/home/components/users/users.component.ts
+++ 5º Semestre/Desenvolvimento-web/crud-app-angular - AC2/src/app/pages/home/components/users/users.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class UsersComponent {
 
+  user = [
+    { nome: 'Ana', email: 'ana@example.com', senha: 'senha123', status: 'Ativo', editing: false },
+    { nome: 'Carlos', email: 'carlos@example.com', senha: 'senha123', status: 'Ativo', editing: false },
+    { nome: 'Diego', email: 'carlos@example.com', senha: 'senha123', status: 'Ativo', editing: false },
+    { nome: 'Leonardo', email: 'carlos@example.com', senha: 'senha123', status: 'Ativo', editing: false },
+    { nome: 'Maria', email: 'carlos@example.com', senha: 'senha123', status: 'Inativo', editing: false }
+  ];
+
+  botaoAlterarStatus(user: any) {
+    user.status = user.status === 'Ativo' ? 'Inativo' : 'Ativo';
+  }
+
+  editarUsuario(user: any) {
+    user.editing = !user.editing;
+  }
+
+  salvarEdicao(user: any) {
+    user.editing = false;
+  }
+
+  deletarUsuario(user: any) {
+    const index = this.user.indexOf(user);
+    if (index !== -1) {
+      this.user.splice(index, 1);
+    }
+  }
+  incluirUsuario() {
+    const novoUsuario = {
+      nome: '',
+      email: '',
+      senha: '',
+      status: 'Ativo',
+      editing: true
+    };
+    this.user.push(novoUsuario);
+  }
+
 }
