f570d663e9f4a19f8cc3b10070febc377dd8efe2
Leonardo Rossi <83621860+leorossiio@users.noreply.github.com>
2024-04-14 18:03:43 -0300

    Adiciona deletarTarefa e NovaTarefa

--- 5º Semestre/Desenvolvimento-web/crud-app-angular - AC2/src/app/pages/home/components/tarefas/tarefas.component.ts
+++ 5º Semestre/Desenvolvimento-web/crud-app-angular - AC2/src/app/pages/home/components/tarefas/tarefas.component.ts
@@ -8,28 +8,42 @@ import { Component } from '@angular/core';
 export class TarefasComponent {
   
   task = [
-    { description: 'Tarefa 1', status: 'Não concluída', editing: false },
-    { description: 'Tarefa 2', status: 'Não concluída', editing: false },
-    { description: 'Tarefa 3', status: 'Não concluída', editing: false },
-    { description: 'Tarefa 4', status: 'Não concluída', editing: false },
-    { description: 'Tarefa 5', status: 'Não concluída', editing: false }
+    { description: 'Tarefa 1', status: 'Não iniciado', editing: false },
+    { description: 'Tarefa 2', status: 'Não iniciado', editing: false },
+    { description: 'Tarefa 3', status: 'Não iniciado', editing: false },
+    { description: 'Tarefa 4', status: 'Não iniciado', editing: false },
+    { description: 'Tarefa 5', status: 'Não iniciado', editing: false }
   ];
 
-  botaoConcluir(task: any){
-    if (task.status === 'Não concluída') {
-      task.status = 'Realizando';
-    } else if (task.status === 'Realizando') {
-      task.status = 'Concluída';
-    } else {
-      task.status = 'Não concluída';
+  botaoConcluir(task: any) {
+    if (task.status === 'Não iniciado') {
+      task.status = 'Em andamento';
+    } else if (task.status === 'Em andamento') {
+      task.status = 'Concluído';
+    } else if (task.status === 'Concluído') {
+      task.status = 'Não iniciado';
     }
   }
-
   editarTarefa(task: any) {
     task.editing = !task.editing;
   }
 
   salvarEdicao(task: any) {
-    task.editing = false; // Saia do modo de edição
+    task.editing = false;
+  }
+
+  deletarTarefa(task: any) {
+    const index = this.task.indexOf(task);
+    if (index !== -1) {
+      this.task.splice(index, 1);
+    }
+  }
+  incluirTarefa() {
+    const novaTarefa = {
+        description: '',
+        status: 'Não iniciado',
+        editing: true
+    };
+    this.task.push(novaTarefa);
   }
 }
